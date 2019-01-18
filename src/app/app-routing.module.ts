882ce6be2d8c3396dd3a4796fa1cb858c79ed6ea
Joao Ribeiro <joao.ribeiro@eproseed.com>
2019-01-18 16:28:25 +0000

    Added some updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IcFormComponent } from './ic-form/ic-form.component';
+import { TutorialComponent } from './tutorial/tutorial.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { VersionsComponent } from './versions/versions.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'main', pathMatch: 'full'},
+  { path: 'main', component: IcFormComponent},
+  { path: 'tutorial', component: TutorialComponent},
+  { path: 'navbar', component: NavbarComponent},
+  { path: 'versions', component: VersionsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [
+  IcFormComponent,
+  TutorialComponent,
+  NavbarComponent,
+  VersionsComponent,
+]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IntroComponent } from './intro/intro.component';
-import { IcFormComponent } from './ic-form/ic-form.component';
 import { XmlPipe } from './ic-form/ic-form.component';
+import { VersionsComponent } from './versions/versions.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IntroComponent,
-    IcFormComponent,
-    XmlPipe
+    XmlPipe,
+    routingComponents,
+    VersionsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
